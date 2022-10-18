60a294dd7cdda2f564b9c76c8694d32c13a98fed
Ritesh Kumar Khandelwal <ritesh@Riteshs-MacBook-Air.local>
2022-10-18 11:31:54 +0530

    utility functions for logged in api calls

--- src/functions/apiClient.js
+++ src/functions/apiClient.js
@@ -1,6 +1,6 @@
 // import { result } from "lodash";
 import { API_BASE_URL, AUTH_URL, LOCAL_BASE_URL } from "../config";
-
+const token = localStorage.getItem("token");
 export function getAuthToken() {
   return fetch(`${AUTH_URL}${"/GetConfig.php"}`)
     .then((response) => response.json())
@@ -51,11 +51,48 @@ export function getData(url, authKey, localURL) {
 }
 
 export const getAuthData = async (url) => {
-  const token = await getAuthToken();
-  return await getData(url, token);
+  const tokenData = await getAuthToken();
+  return await getData(url, tokenData);
 };
 
 export const getAuthDataPost = async (url, data) => {
-  const token = await getAuthToken();
-  return await postData(url, data, token);
+  const tokenData = await getAuthToken();
+  return await postData(url, data, tokenData);
+};
+
+export const getRequestLoggedIn = (url) => {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if (token) {
+    myHeaders.append("auth_token", token);
+  }
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return fetch(`${LOCAL_BASE_URL}${url}`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((error) => error);
+};
+
+export const postRequestLoggedIn = (url, data) => {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if (token) {
+    myHeaders.append("auth_token", token);
+  }
+  var raw = JSON.stringify(data);
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  return fetch(`${LOCAL_BASE_URL}${url}`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((error) => error);
 };
