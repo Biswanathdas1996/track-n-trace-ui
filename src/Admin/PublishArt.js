57efc60877b8391e08c6b9497e27393ac14dc9be
Ritesh Kumar Khandelwal <ritesh@Riteshs-MacBook-Air.local>
2022-10-20 10:56:00 +0530

    tokens tab

--- src/Admin/PublishArt.js
+++ src/Admin/PublishArt.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Formik, Form, Field, FieldArray } from "formik";
-// import * as Yup from "yup";
+import React, { useState, useEffect } from "react";
+import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { Card, Grid } from "@mui/material";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
@@ -8,36 +8,55 @@ import TextareaAutosize from "@mui/material/TextareaAutosize";
 import DeleteOutlineIcon from "@mui/icons-material/Delete";
 import { pink } from "@mui/material/colors";
 import TransctionModal from "../components/shared/TransctionModal";
-import { getAuthDataPost } from "../functions/apiClient";
+import { getAuthDataPost, getRequestLoggedIn } from "../functions/apiClient";
 
-// const VendorSchema = Yup.object().shape({
-//   name: Yup.string().required("Name is required"),
-//   authorname: Yup.string().required("Authorname is required"),
-//   price: Yup.string().required("Price is required"),
-//   royelty: Yup.string().required("Royelty amount is required"),
+// const validationSchema = Yup.object().shape({
+//   batchNumber: Yup.string().required("Batch Number is required"),
+//   title: Yup.string().required("Title is required"),
+//   description: Yup.string().required("Description is required"),
+//   attributes: Yup.string().required("Attributes are required"),
 // });
 
 const Mint = () => {
   const [start, setStart] = useState(false);
   const [response, setResponse] = useState(null);
+  const [cat, setCat] = useState();
+  const [subCat, setSubCat] = useState();
 
-  const [description, setDescription] = useState(null);
+  const [catArray, setCatArray] = useState([]);
+  const [subCatArray, setSubCatArray] = useState([]);
+  const [productArray, setProductArray] = useState([]);
 
+  useEffect(() => {
+    const catDetails = async () => {
+      const res = await getCategoryList();
+      setCatArray(res);
+    };
+    catDetails();
+  }, []);
   let history = useNavigate();
 
-  const saveData = async ({ title, category, attributes }) => {
+  const saveData = async ({
+    title,
+    product,
+    attributes,
+    batchNumber,
+    description,
+  }) => {
     setStart(true);
     let responseData;
 
     const metaData = {
       name: title,
-      product: category,
+      category: cat,
+      subCategory: subCat,
+      product: product,
       image: null,
       description: description,
       attributes: attributes,
+      batchNumber: batchNumber,
       transction: [],
     };
-
     await getAuthDataPost(`/create`, metaData);
     history("/dashboard");
 
@@ -49,6 +68,34 @@ const Mint = () => {
     setResponse(null);
     history("/dashboard");
   };
+  const getCategoryList = async () => {
+    const res = await getRequestLoggedIn("/categoryList");
+    if (res?.status_code === "200") {
+      return res.categoryList.map((obj) => obj);
+    }
+    return null;
+  };
+
+  const getSubCategoryList = async (catId) => {
+    const res = await getRequestLoggedIn(`/sub_categoryList?cat_id=${catId}`);
+    if (res?.status_code === "200") {
+      return (
+        res && res.sub_categoryList && res.sub_categoryList.map((obj) => obj)
+      );
+    }
+    return null;
+  };
+
+  const getProductList = async (catId, subCatId) => {
+    const res = await getRequestLoggedIn(
+      `/productList?cat_id=${catId}&subcat_id=${subCatId}`
+    );
+    if (res?.status_code === "200") {
+      return res && res.productList && res.productList.map((obj) => obj);
+    }
+    return null;
+  };
+
   return (
     <>
       {start && <TransctionModal response={response} modalClose={modalClose} />}
@@ -74,7 +121,7 @@ const Mint = () => {
                         category: "",
                         attributes: [],
                       }}
-                      // validationSchema={VendorSchema}
+                      // validationSchema={validationSchema}
                       onSubmit={(values, { setSubmitting }) => {
                         saveData(values);
                         setSubmitting(false);
@@ -101,15 +148,22 @@ const Mint = () => {
                                       : ""
                                   }`}
                                   style={{ marginRight: 10, padding: 9 }}
+                                  onChange={async (e) => {
+                                    setCat(e.target.value);
+                                    const res = await getSubCategoryList(
+                                      e.target.value
+                                    );
+                                    setSubCatArray(res);
+                                  }}
+                                  value={cat}
                                 >
                                   <option>-- Please select --</option>
-                                  {/* end pint fetching from getCategory */}
-
-                                  <option value="Lamborghini">
-                                    Lamborghini
-                                  </option>
-                                  <option value="Audi">Audi</option>
-                                  <option value="BMW">BMW</option>
+                                  {catArray &&
+                                    catArray.map((item) => (
+                                      <option value={item.category_id}>
+                                        {item.category_name}
+                                      </option>
+                                    ))}
                                 </Field>
                               </div>
                             </Grid>
@@ -123,22 +177,32 @@ const Mint = () => {
                                   <span className="text-danger">*</span>
                                 </label>
                                 <Field
-                                  name="sub category"
+                                  name="subCategory"
                                   component="select"
                                   className={`form-control text-muted ${
-                                    touched.subcategory && errors.subcategory
+                                    touched.subCategory && errors.subCategory
                                       ? "is-invalid"
                                       : ""
                                   }`}
                                   style={{ marginRight: 10, padding: 9 }}
+                                  onChange={async (e) => {
+                                    setSubCat(e.target.value);
+                                    const res = await getProductList(
+                                      cat,
+                                      e.target.value
+                                    );
+                                    setProductArray(res);
+                                  }}
+                                  disabled={!cat}
+                                  value={subCat}
                                 >
                                   <option>-- Please select --</option>
-
-                                  <option value="Lamborghini">
-                                    Lamborghini
-                                  </option>
-                                  <option value="Audi">Audi</option>
-                                  <option value="BMW">BMW</option>
+                                  {subCatArray &&
+                                    subCatArray.map((item) => (
+                                      <option value={item.sub_category_id}>
+                                        {item.subcategory_name}
+                                      </option>
+                                    ))}
                                 </Field>
                               </div>
                             </Grid>
@@ -159,15 +223,41 @@ const Mint = () => {
                                       : ""
                                   }`}
                                   style={{ marginRight: 10, padding: 9 }}
+                                  disabled={!subCat}
                                 >
                                   <option>-- Please select --</option>
 
-                                  <option value="Lamborghini">
-                                    Lamborghini
-                                  </option>
-                                  <option value="Audi">Audi</option>
-                                  <option value="BMW">BMW</option>
+                                  {productArray &&
+                                    productArray.map((item) => (
+                                      <option value={item.productid}>
+                                        {item.product_name}
+                                      </option>
+                                    ))}
                                 </Field>
+                              </div>
+                            </Grid>
+                            <Grid item lg={12} md={12} sm={12} xs={12}>
+                              <div
+                                className="form-group"
+                                style={{ marginLeft: 10, marginTop: 10 }}
+                              >
+                                <label htmlFor="title" className="my-2">
+                                  Batch Number{" "}
+                                  <span className="text-danger">*</span>
+                                </label>
+                                <Field
+                                  type="text"
+                                  name="batchNumber"
+                                  autoComplete="false"
+                                  placeholder="Enter Batch Number"
+                                  className={`form-control text-muted ${
+                                    touched.batchNumber && errors.batchNumber
+                                      ? "is-invalid"
+                                      : ""
+                                  }`}
+                                  style={{ marginRight: 10, padding: 9 }}
+                                />
+                                {/* <ErrorMessage name={"batchNumber"} /> */}
                               </div>
                             </Grid>
                             <Grid item lg={12} md={12} sm={12} xs={12}>
@@ -182,7 +272,7 @@ const Mint = () => {
                                 <Field
                                   type="text"
                                   name="title"
-                                  autoComplete="flase"
+                                  autoComplete="false"
                                   placeholder="Enter title"
                                   className={`form-control text-muted ${
                                     touched.title && errors.title
@@ -192,6 +282,7 @@ const Mint = () => {
                                   style={{ marginRight: 10, padding: 9 }}
                                 />
                               </div>
+                              {/* <ErrorMessage name={"title"} /> */}
                             </Grid>
 
                             <Grid item lg={12} md={12} sm={12} xs={12}>
@@ -199,25 +290,24 @@ const Mint = () => {
                                 className="form-group"
                                 style={{ marginLeft: 10, marginTop: 10 }}
                               >
-                                <label htmlFor="title" className="my-2">
+                                <label htmlFor="description" className="my-2">
                                   Description{" "}
                                   <span className="text-danger">*</span>
                                 </label>
                                 <TextareaAutosize
+                                  type="text"
                                   aria-label="minimum height"
                                   minRows={3}
-                                  name="text"
-                                  onChange={(e) =>
-                                    setDescription(e.target.value)
-                                  }
+                                  name="description"
                                   placeholder="Please enter some descriptions"
                                   style={{ width: "100%" }}
                                   className={`form-control text-muted ${
-                                    touched.text && errors.text
+                                    touched.description && errors.description
                                       ? "is-invalid"
                                       : ""
                                   }`}
                                 />
+                                {/* <ErrorMessage name="description" /> */}
                               </div>
                             </Grid>
 
@@ -265,7 +355,7 @@ const Mint = () => {
                                                 >
                                                   <Field
                                                     name={`attributes.${index}.trait_type`}
-                                                    autoComplete="flase"
+                                                    autoComplete="false"
                                                     placeholder="Enter Properties name"
                                                     className={`form-control text-muted `}
                                                     style={{
@@ -283,7 +373,7 @@ const Mint = () => {
                                                 >
                                                   <Field
                                                     name={`attributes.${index}.value`}
-                                                    autoComplete="flase"
+                                                    autoComplete="false"
                                                     placeholder="Enter value"
                                                     className={`form-control text-muted`}
                                                     style={{
@@ -293,6 +383,9 @@ const Mint = () => {
                                                   />
                                                 </Grid>
                                               </Grid>
+                                              {/* <ErrorMessage
+                                                name={"attribute"}
+                                              /> */}
                                             </div>
                                           )
                                         )
